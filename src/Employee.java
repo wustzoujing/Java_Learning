2880eb4210cd766b4dd077cf9e4210582a1da64b
wustzoujing <164325433@qq.com>
2017-05-11 10:09:59 +0800

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,20 @@
+/**
+ * Created by zoujing on 2017/5/10.
+ *
+ */
+
+    //默认也会继承object，这里就证明toString()方法被调用了。
+    public class Employee extends Object {
+        private String name;
+        public Employee(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+        public static void main(String[] args) {
+            Employee emp = new Employee("John Doe");
+            System.out.println(emp.getName());
+        }
+    }
+
